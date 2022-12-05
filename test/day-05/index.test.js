8bddfd2f1582ff2616a20e412d322f1121d83b63
Chris Piccaro <chrispiccaro18@gmail.com>
2022-12-05 13:28:25 -0500

    solves day05 part 02

--- test/day-05/index.test.js
+++ test/day-05/index.test.js
@@ -1,4 +1,4 @@
-const { day05part01 } = require('../../lib/day-05');
+const { day05part01, day05part02 } = require('../../lib/day-05');
 const { testInput, actualInput } = require('../../lib/day-05/input');
 
 describe('day 05', () => {
@@ -10,6 +10,16 @@ describe('day 05', () => {
     it('Solves the part01 actual input', () => {
         const result = day05part01(actualInput);
         expect(result).toBe('QNHWJVJZW');
+    });
+    
+    it('Solves the part02 test input', () => {
+        const result = day05part02(testInput);
+        expect(result).toBe('MCD');
+    });
+    
+    it('Solves the part02 actual input', () => {
+        const result = day05part02(actualInput);
+        expect(result).toBe('BPCZJLFJW');
     });
 
 });
