e9a9dd03cf7c12771ae89c86b2f260964a3b74fe
Chris Piccaro <chrispiccaro18@gmail.com>
2022-12-06 11:00:34 -0500

    solves day06 part02

--- test/day06/index.test.js
+++ test/day06/index.test.js
@@ -1,4 +1,4 @@
-const { day06part01 } = require('../../lib/day-06');
+const { day06part01, day06part02 } = require('../../lib/day-06');
 const { testInputs, actualInput } = require('../../lib/day-06/input');
 
 describe('day 06', () => {
@@ -18,14 +18,20 @@ describe('day 06', () => {
         expect(result).toBe(1140);
     });
     
-    // it('Solves the part02 test input', () => {
-    //     const result = day05part02(testInput);
-    //     expect(result).toBe('MCD');
-    // });
+    it('Solves the part01 test input', () => {
+        const result = testInputs.map(testInput => day06part02(testInput));
+        expect(result).toEqual([
+            19,
+            23,
+            23,
+            29,
+            26,
+        ]);
+    });
     
-    // it('Solves the part02 actual input', () => {
-    //     const result = day05part02(actualInput);
-    //     expect(result).toBe('BPCZJLFJW');
-    // });
+    it('Solves the part02 actual input', () => {
+        const result = day06part02(actualInput);
+        expect(result).toBe(3495);
+    });
 
 });
