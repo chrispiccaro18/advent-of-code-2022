2b4fadb152f1d733d450642e593a436cb3d16c08
Chris Piccaro <chrispiccaro18@gmail.com>
2022-12-07 21:10:40 -0500

    dirs are not unique, recursion error

--- test/day-07/index.test.js
+++ test/day-07/index.test.js
@@ -1,31 +1,79 @@
-const { day07part01 } = require('../../lib/day-07');
-const { testInput } = require('../../lib/day-07/input');
+const { day07part01, calculateDirSize, calculateFilesInDir } = require('../../lib/day-07');
+const { testInput, actualInput } = require('../../lib/day-07/input');
 
 describe('day 07', () => {
-    it('Solves the part01 test input', () => {
+    it('Calculates file size in dir', () => {
+        const dir = {
+            name: 'd',
+            parent: '/',
+            children: [],
+            files: ['j', 'd.log', 'd.ext', 'k']
+        };
+        const files = {
+            'b.txt': { name: 'b.txt', parent: '/', size: 14848514 },
+            'c.dat': { name: 'c.dat', parent: '/', size: 8504156 },
+            f: { name: 'f', parent: 'a', size: 29116 },
+            g: { name: 'g', parent: 'a', size: 2557 },
+            'h.lst': { name: 'h.lst', parent: 'a', size: 62596 },
+            i: { name: 'i', parent: 'e', size: 584 },
+            j: { name: 'j', parent: 'd', size: 4060174 },
+            'd.log': { name: 'd.log', parent: 'd', size: 8033020 },
+            'd.ext': { name: 'd.ext', parent: 'd', size: 5626152 },
+            k: { name: 'k', parent: 'd', size: 7214296 }
+        };
+        const result = calculateFilesInDir(dir, files);
+        expect(result).toBe(24933642);
+    });
+
+    it('Calculates directory sizes', () => {
+        const dir = {
+            name: 'a',
+            parent: '/',
+            children: [
+                'e',
+            ],
+            files: ['f', 'g', 'h.lst']
+        };
+
+        const dirTree = {
+            '/': { name: '/', children: ['a', 'd'], files: ['b.txt', 'c.dat'] },
+            files: {
+                'b.txt': { name: 'b.txt', parent: '/', size: 14848514 },
+                'c.dat': { name: 'c.dat', parent: '/', size: 8504156 },
+                f: { name: 'f', parent: 'a', size: 29116 },
+                g: { name: 'g', parent: 'a', size: 2557 },
+                'h.lst': { name: 'h.lst', parent: 'a', size: 62596 },
+                i: { name: 'i', parent: 'e', size: 584 },
+                j: { name: 'j', parent: 'd', size: 4060174 },
+                'd.log': { name: 'd.log', parent: 'd', size: 8033020 },
+                'd.ext': { name: 'd.ext', parent: 'd', size: 5626152 },
+                k: { name: 'k', parent: 'd', size: 7214296 }
+            },
+            a: {
+                name: 'a',
+                parent: '/',
+                children: ['e'],
+                files: ['f', 'g', 'h.lst']
+            },
+            d: {
+                name: 'd',
+                parent: '/',
+                children: [],
+                files: ['j', 'd.log', 'd.ext', 'k']
+            },
+            e: { name: 'e', parent: 'a', children: [], files: ['i'] }
+        };
+        const result = calculateDirSize(dir, dirTree);
+        expect(result).toBe(94853);
+    });
+
+    it.only('Solves the part01 test input', () => {
         const result = day07part01(testInput);
         expect(result).toBe(95437);
     });
     
-    // it('Solves the part01 actual input', () => {
-    //     const result = day06part01(actualInput);
-    //     expect(result).toBe(1140);
-    // });
-    
-    // it('Solves the part01 test input', () => {
-    //     const result = testInputs.map(testInput => day06part02(testInput));
-    //     expect(result).toEqual([
-    //         19,
-    //         23,
-    //         23,
-    //         29,
-    //         26,
-    //     ]);
-    // });
-    
-    // it('Solves the part02 actual input', () => {
-    //     const result = day06part02(actualInput);
-    //     expect(result).toBe(3495);
-    // });
-
+    it.only('Solves the part01 actual input', () => {
+        const result = day07part01(actualInput);
+        expect(result).toBe(1140);
+    });
 });
